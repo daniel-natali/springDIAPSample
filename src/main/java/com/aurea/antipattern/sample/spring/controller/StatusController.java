cf6f1fd0379fff195464aa5f5723d1edecd52568
daniel.natali <daniel.natali@aurea.com>
2017-05-09 17:16:06 -0300

    Added mime types

--- src/main/java/com/aurea/antipattern/sample/spring/controller/StatusController.java
+++ src/main/java/com/aurea/antipattern/sample/spring/controller/StatusController.java
@@ -1,6 +1,7 @@
 package com.aurea.antipattern.sample.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class StatusController {
   private StatusService statusService;
   
   
-  @RequestMapping("/allstatus")
+  @RequestMapping(value="/allstatus", produces={MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE, "text/yaml"} )
   public Iterable<Status> allstatus() {
       return statusService.getStatusForAllComponents();
   }
